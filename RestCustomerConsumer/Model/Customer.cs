ec4af2a5b17ab0de8877445791e0f80929911025
Sebastian Petersen <sebastian@srmail.dk>
2018-10-07 19:34:07 +0200

    Added Json support. Fixed namespace. Added method for HTTP GET.

---
+++ RestCustomerConsumer/Model/Customer.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace RestCustomerConsumer.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Year { get; set; }
+
+        public Customer(int id, string firstName, string lastName, int year)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Year = year;
+        }
+        public Customer(){}
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(FirstName)}: {FirstName}, {nameof(LastName)}: {LastName}, {nameof(Year)}: {Year}";
+        }
+    }
+}
--- RestCustomerConsumer/Program.cs
+++ RestCustomerConsumer/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestCustomerConsumer.Model;
 
 namespace RestCustomerConsumer
 {
     class Program
     {
+        private static readonly string CustomersUri = "https://restcustomerservice20181007065419.azurewebsites.net/api/Customers";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (var c in GetCustomersAsync().Result)
+            {
+                System.Console.WriteLine(c);
+            }
+        }
+        public static async Task<IList<Customer>> GetCustomersAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync(CustomersUri);
+                IList<Customer> cList = JsonConvert.DeserializeObject<IList<Customer>>(content);
+                return cList;
+            }
         }
     }
 }
