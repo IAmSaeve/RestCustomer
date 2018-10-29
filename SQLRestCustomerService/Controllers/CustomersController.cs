c9c95989c1e31019c55931a412f150b727f78e9e
Sebastian Petersen <sebastian@srmail.dk>
2018-10-29 20:28:33 +0100

    Added code for GET, PUT, POST, DELETE for MySQL.

--- SQLRestCustomerService/Controllers/CustomersController.cs
+++ SQLRestCustomerService/Controllers/CustomersController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using SQLRestCustomerService.Model;
 using MySqlConnector;
-using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace SQLRestCustomerService.Controllers
 {
@@ -14,41 +14,88 @@ namespace SQLRestCustomerService.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
-        
+        private const string connection = "server=192.168.122.46; uid=root; pwd=password; database=CustomerDB";
         
         // GET: api/Customers
         [HttpGet]
         public List<Customer> Get()
         {
-            return null;
+            var result = new List<Customer>();
+            var sql = "SELECT * FROM Customer";
+            var db = new MySqlConnection(connection);
+            db.Open();
+
+            var command = new MySqlCommand(sql, db);
+            var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    result.Add(new Customer(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetInt32(3)));
+                }
+            }
+            return result;
         }
 
         // GET: api/Customers/1
         [HttpGet("{id}")]
         public Customer Get(int id)
         {
-            return null;
+            Customer result = null;
+            var sql = $"SELECT * FROM Customer WHERE id = '{id}'";
+            var db = new MySqlConnection(connection);
+            db.Open();
+
+            var command = new MySqlCommand(sql, db);
+            var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    result = new Customer(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetInt32(3));
+                }
+            }
+            return result;
         }
 
         // POST api/Customers
         [HttpPost]
         public void InsertCustomer(Customer customer)
         {
-            
+            var sql = "INSERT INTO Customer (id, firstName, lastName, cYear)" +
+            $"VALUES (NULL, '{customer.FirstName}', '{customer.LastName}', '{customer.Year}')";
+            var db = new MySqlConnection(connection);
+            db.Open();
+
+            var command = new MySqlCommand(sql, db);
+            var reader = command.ExecuteReader();
         }
 
         // PUT api/Customers/5
         [HttpPut("{id}")]
         public void UpdateCustomer(int id, [FromBody] Customer customer)
         {
-            
+            var sql = $"UPDATE Customer SET firstName = '{customer.FirstName}', " +
+            $"lastName = '{customer.LastName}', cYear = '{customer.Year}' WHERE id='{id}'";
+            var db = new MySqlConnection(connection);
+            db.Open();
+
+            var command = new MySqlCommand(sql, db);
+            var reader = command.ExecuteReader();
         }
 
         // DELETE api/Customers/5
         [HttpDelete("{id}")]
         public void DeleteCustomer(int id)
         {
-            
+            var sql = $"DELETE FROM Customer WHERE id='{id}'";
+            var db = new MySqlConnection(connection);
+            db.Open();
+
+            var command = new MySqlCommand(sql, db);
+            var reader = command.ExecuteReader();
         }
     }
 }
